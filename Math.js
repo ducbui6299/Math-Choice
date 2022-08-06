4bac7367ebecc2e0a8c78152c296469cfce3bab0
Duc Bui <ducbui6299@gmail.com>
2022-08-06 16:44:50 +0700

    dang fix

--- Math.js
+++ Math.js
@@ -8,13 +8,14 @@ const bear2_screen2 = document.getElementById("bear2_screen2");
 const bear_screen2 = document.getElementById("bear_screen2");
 const rabit_screen2 = document.getElementById("rabit_screen2");
 const btn_continue = document.getElementById("btn_continue");
+const normalCharacter = document.getElementById("normalCharacter");
 
-function cancelEvent(){
-  fox_screen2.style.pointerEvents="none"
-  bear2_screen2.style.pointerEvents="none"
-  rabit_screen2.style.pointerEvents="none"
-  bear_screen2.style.pointerEvents="none"
-  Screen2_background.style.pointerEvents="none"
+function cancelEvent() {
+  fox_screen2.style.pointerEvents = "none";
+  bear2_screen2.style.pointerEvents = "none";
+  rabit_screen2.style.pointerEvents = "none";
+  bear_screen2.style.pointerEvents = "none";
+  Screen2_background.style.pointerEvents = "none";
 }
 
 // Fox action
@@ -29,7 +30,9 @@ fox_screen2.addEventListener("click", (e) => {
   bear_screen2.src = "./MathImg/Nor-Slect-Option-Bear.png";
   rabit_screen2.src = "./MathImg/Nor-Slect-Option-rabbit.png";
   bear2_screen2.src = "./MathImg/Nor-Slect-Option-Bear2.png";
-  cancelEvent()
+  cancelEvent();
+  // normalCharacter.src = "./MathImg/Nor-study-Fox.png";
+
 });
 fox_screen2.addEventListener("mouseover", (e) => {
   fox_name.style.color = "#FFFB93";
@@ -44,7 +47,6 @@ fox_screen2.addEventListener("mouseover", (e) => {
 
 // Rabit action
 rabit_screen2.addEventListener("click", (e) => {
-  console.log("dang click vao cao.");
   rabit_name.style.color = "#B5FF93";
   fox_name.style.color = "#FFFFFF";
   bear_name.style.color = "#FFFFFF";
@@ -54,7 +56,9 @@ rabit_screen2.addEventListener("click", (e) => {
   bear_screen2.src = "./MathImg/Nor-Slect-Option-Bear.png";
   rabit_screen2.src = "./MathImg/Selected-select-option-rabbit.png";
   bear2_screen2.src = "./MathImg/Nor-Slect-Option-Bear2.png";
-  cancelEvent()
+  localStorage.setItem('noramlAva',"./MathImg/Nor-study-rabbit.png")
+  cancelEvent();
+  // normalCharacter.src = "./MathImg/Nor-study-rabbit.png";
 });
 rabit_screen2.addEventListener("mouseover", (e) => {
   rabit_name.style.color = "#FFFB93";
@@ -78,7 +82,6 @@ bear_screen2.addEventListener("mouseover", (e) => {
   bear2_screen2.src = "./MathImg/Nor-study-Bear2.png";
 });
 bear_screen2.addEventListener("click", (e) => {
-  console.log("dang click vao cao.");
   rabit_name.style.color = "#FFFFFF";
   fox_name.style.color = "#FFFFFF";
   bear2_name.style.color = "#FFFFFF";
@@ -88,8 +91,10 @@ bear_screen2.addEventListener("click", (e) => {
   bear_screen2.src = "./MathImg/Selected-select-option-bear.png";
   rabit_screen2.src = "./MathImg/Nor-Slect-Option-rabbit.png";
   bear2_screen2.src = "./MathImg/Nor-Slect-Option-Bear2.png";
-  cancelEvent()
+  cancelEvent();
+  // normalCharacter.src = "./MathImg/Nor-study-Bear2.png";
 });
+
 
 //Bear2 action
 bear2_screen2.addEventListener("mouseover", (e) => {
@@ -103,7 +108,6 @@ bear2_screen2.addEventListener("mouseover", (e) => {
   bear_screen2.src = "./MathImg/Nor-study-Bear.png";
 });
 bear2_screen2.addEventListener("click", (e) => {
-  console.log("dang click vao cao.");
   rabit_name.style.color = "#FFFFFF";
   fox_name.style.color = "#FFFFFF";
   bear_name.style.color = "#FFFFFF";
@@ -113,5 +117,7 @@ bear2_screen2.addEventListener("click", (e) => {
   bear2_screen2.src = "./MathImg/Selected-select-option-bear2.png";
   rabit_screen2.src = "./MathImg/Nor-Slect-Option-rabbit.png";
   bear_screen2.src = "./MathImg/Nor-Slect-Option-Bear.png";
-  cancelEvent()
+  cancelEvent();
+  // normalCharacter.src = "./MathImg/Nor-study-Bear.png";
 });
+console.log(localStorage.getItem('noramlAva'))
