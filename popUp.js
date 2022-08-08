043229c08b4cad93b3a6bfc9596ded8c90625343
Duc Bui <ducbui6299@gmail.com>
2022-08-08 23:53:34 +0700

    done pop up

--- popUp.js
+++ popUp.js
@@ -6,10 +6,11 @@ const close_popup_btn=document.getElementById('close_popup_btn')
 function showPopUp(){
     blurBG.style.display='block'
     infoPopUp.style.display='block'
+    console("dang bat popup")
 }
 function closePopUp(){
     blurBG.style.display='none'
     infoPopUp.style.display='none'
 }
-btn_info.addEventListener('click',showPopUp())
-close_popup_btn.addEventListener('click',closePopUp())
+btn_info.addEventListener('click',showPopUp)
+close_popup_btn.addEventListener('click',closePopUp)
